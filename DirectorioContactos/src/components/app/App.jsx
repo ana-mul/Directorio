99220f81bc5add245c7976880d0b245a8a786e0c
Ana Muller <anamullerpsi@gmail.com>
2024-02-24 19:34:33 -0300

    Directorio

--- DirectorioContactos/src/components/app/App.jsx
+++ DirectorioContactos/src/components/app/App.jsx
@@ -12,15 +12,19 @@ function App() {
   const [email, setEmail] = useState("");
   const [notes, setNotes] = useState("");
 
-
   const [filter, setFilter] = useState("");
 
-  const randomID = () => {
-    return Math.floor(Math.random() * 1000000);
+  const inputName = (event) => {
+    setName(event.target.value);
   };
-
-  const inputEvent = (event) => {
-    setAddContact(event.target.value);
+  const inputPhone = (event) => {
+    setPhone(event.target.value);
+  };
+  const inputEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const inputNotes = (event) => {
+    setNotes(event.target.value);
   };
 
   const add = () => {
@@ -28,26 +32,26 @@ function App() {
       //aca controlo que no este libre el input
       //.trim ayuda a que la funcion no tome en cuenta espacios en blanco
       //no agrego notes en el condicional porque no quiero que sea obligatoria
-      const newContact ={
+      const newContact = {
         name: name,
         phone: phone,
         email: email,
-        notes: notes
+        notes: notes,
       }; //si no esta libre, creo el contacto (este pasaria a se el estado actual)
-    
+
       const adding = [...contact, newContact]; //se crea una varible que sume el nuevo contacto a la lista anterior
       setContact(adding); //actualizo el estado que contiene el array
       setName(""); //vacio el input
-      setPhone ("");
+      setPhone("");
       setEmail("");
       setNotes("");
-    };
+    }
   };
 
   return (
     <>
       <div>
-        <input type="text" onChange={inputEvent} value={addName} />
+        <input type="text" onChange={inputName} value={addName} />
         <button onClick={add}>submit</button>
       </div>
     </>
